780d81827065fec97e8626b61602877531f83840
YanTylets <72673268+YanTylets@users.noreply.github.com>
2021-03-14 01:27:27 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 
 // You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+function towelSort (matrix) {
+  for (i=1; i < matrix.length; i++) {
+      matrix[i] = matrix[i].reverse();
+  }
+  return matrix.flat();
+};
+alert (towelSort([
+  [ 1, 2, 3 ],
+  [ 4, 5, 6 ],
+  [ 7, 8, 9 ],
+]));
